82d000f40fafb0322f9f79ae664363f90b3be11d
xbenduan <128107753+xbenduan@users.noreply.github.com>
2026-06-09 11:21:40 +0800

    refactor(cms): remove index.ts

--- packages/cms/src/provider/tcb/index.ts
+++
@@ -1,5 +0,0 @@
-export { createTcbClient } from "./tcb-client";
-export type { TcbClientOptions, TcbClient } from "./tcb-client";
-export { TcbSchemaProvider } from "./tcb-schema-provider";
-export { TcbRecordProvider } from "./tcb-record-provider";
-export { TcbLogProvider } from "./tcb-log-provider";
